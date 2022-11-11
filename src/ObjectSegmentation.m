7ea54aeafea2712938667d51f22f1e87eae21c04
dethaa <sharonmarbun12@gmail.com>
2022-11-11 13:29:15 +0700

    fix: getSegmentationMask + configuring initial threshold in gui

--- src/ObjectSegmentation.m
+++ src/ObjectSegmentation.m
@@ -3,10 +3,10 @@ classdef ObjectSegmentation
         function res = threshold(image, threshold_algorithm, T_input)
             if (threshold_algorithm == "Selected Threshold")
                 T = T_input/255;
-                res = im2bw(image, T);
+                res = imbinarize(image, T);
             elseif (threshold_algorithm == "Otsu")
                 T =  graythresh(image);
-                res = im2bw(image, T);
+                res = imbinarize(image, T);
             elseif (threshold_algorithm == "Adaptive")
                 res = imbinarize(image,'adaptive');
             end
@@ -17,20 +17,24 @@ classdef ObjectSegmentation
             bwimg = ObjectSegmentation.threshold(image, threshold_algorithm, T_input);
             
             % get segmentation mask
-            res = imclearborder(bwimg);
-            res = imclose(res, strel('disk',5));
-            res = bwareaopen(res, 50);
+            %res = imclose(res, strel('disk',5));
+            %res = imclearborder(bwimg);
+            %res = bwareaopen(res, 50);
             
-            res = imfill(res, 4, 'holes');
-            res = imclose(res, strel('disk',100));
+            %res = imfill(res, 4, 'holes');
+            %res = imclose(res, strel('disk',100));
+            
+            res = imclearborder(bwimg);
+            res = imclose(res, strel('line', 10,0));
+            res = imfill(res, 'holes');
+            res = imopen(res, strel(ones(3,3)));
+            res = bwareaopen(res, 1500);
         end
 
         function res = segmentObject(image, segmask, isGrayscale)
             if (isGrayscale)
-                disp("ya");
                 res(:,:) = image(:,:).*uint8(segmask);
             else
-                disp("tdk");
                 res(:,:,1) = image(:,:,1).*uint8(segmask);
                 res(:,:,2) = image(:,:,2).*uint8(segmask);
                 res(:,:,3) = image(:,:,3).*uint8(segmask);
